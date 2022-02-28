ab93fd0baabe817461dda3c5388232719bc30e18
Lavanyaa K.R <96877605+Lavanyabts@users.noreply.github.com>
2022-02-28 15:14:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,33 @@ const noOfNotes = document.querySelectorAll(".no-of-notes");
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount()
+{
+    hideMessage();
+    if (billAmount.value > 0) {
+        if (cashGiven.value >= billAmount.value) {
+            const amountToBeReturned = cashGiven.value - billAmount.value; // 2022 - 12 = 2010
+            calculateChange(amountToBeReturned);
+        } else {
+            showMessage("Do you wanna wash plates?");
+        }
+    } else {
+        showMessage("Invalid Bill Amount");
+    }
+});
 
-hideMessage();
+function calculateChange(amountToBeReturned) {
+    for (let i = 0; i < availableNotes.length; i++) {
+        
+        const numberOfNotes = Math.trunc(
+            amountToBeReturned / availableNotes[i]
+        );
+       
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
+
+/* hideMessage();
 var bill = Number(billAmount.value);
 var cash = Number(cashGiven.value);
 if (bill > 0) {
@@ -33,38 +58,7 @@ for(let i=0; i<availableNotes.length; i++) {
     returnAmount %= availableNotes[i];
     noOfNotes[i].innerText = numberOfNotes;
 }
-}
-
-
-
-/* {
-  hideMessage();
-  var bill = Number(billAmount.value);
-  var cash = Number(cashGiven.value);
-  if (bill > 0) {
-      if (cash >= bill) {
-          const returnAmount = cash - bill;
-          calculateChange(returnAmount);
-    } else 
-    {
-        showMessage("Do You Wanna Make DosaBater?");
-        calculateChange(0);
-    } 
-} else {
-    showMessage("Invalid Bill Amount");
-    calculateChange(0);
-}
-});
-
-function calculateChange() {
-    for (let i=0; i<availableNotes.length; i++) {
-        const numberOfNotes = Math.trunc(
-            returnAmount / availableNotes[i]
-        );
-        returnAmount %= availableNotes[i];
-      noOfNotes[i].innerText = numberOfNotes;  
-    }
-}  */
+} */
 
 function hideMessage() {
     message.style.display = "none"; 
