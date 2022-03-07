4fe026bc76c71e8a51bbf5c26ee0796cd80e2106
Lavanyaa K.R <96877605+Lavanyabts@users.noreply.github.com>
2022-03-07 17:16:15 +0530

    docs: changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
-const message = document.querySelector("#error-message");
+const Message = document.querySelector("#error-Message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
@@ -61,7 +61,7 @@ for(let i=0; i<availableNotes.length; i++) {
 } */
 
 function hideMessage() {
-    message.style.display = "none"; 
+    Message.style.display = "none"; 
 } 
 
 function showMessage(msg) {
